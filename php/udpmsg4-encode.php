238b9910c1c509e36dc52fe0d8e955079273db7b
Nick <nick@somerandomnick.ano>
2011-10-17 05:11:52 +0000

    added small script that encodes from argv to udpmsg4 to stdout

---
+++ php/udpmsg4-encode.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'libudpmsg4.php';
+
+$p = new udpmsg4_packet;
+
+$argv0 = array_shift($argv);
+
+foreach ($argv as $arg) {
+ list ($key,$value) = explode('=',$arg,2);
+ $p[$key]=$value;
+}
+
+echo $p;
+
+?>
